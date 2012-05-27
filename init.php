267f317e6a62801934c87f46350143888c3dbd38
Isaiah DeRose-Wilson <isaiah@synapsestudios.com>
2012-05-26 17:17:39 -0700

    Update for Kohana 3.3/PSR-0 and remove unused routes from the init file.

--- init.php
+++ init.php
@@ -1,17 +1,3 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
-Route::set('notice-add', 'notice/add/<type>/<message>(/<persist>)')
-	->defaults(array(
-		'controller' => 'notice',
-		'action'     => 'add',
-		'persist'    => FALSE
-	));
-
-Route::set('notice-remove', 'notice/remove/<hash>')
-	->defaults(array(
-		'controller' => 'notice',
-		'action'     => 'remove'
-	));
-
-
 Notices::init();
